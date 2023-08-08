b0718ffead95df2e57c4f68c328d2ff71223ca0a
forapi <kzrvip@gmail.com>
2023-08-08 12:17:02 +0800

    去除(点击购买)

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -124,7 +124,7 @@ export function SideBar(props: { className?: string }) {
         <div className={styles["sidebar-title"]}>ChatGPT Next</div>
         <div className={styles["sidebar-sub-title"]}>
           <a href="https://docs.qq.com/doc/DT0NTUHpIeFNtQUJr" target="_blank">
-            by:梦想技术宅(点击购买)
+            by:梦想技术宅
           </a>
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
